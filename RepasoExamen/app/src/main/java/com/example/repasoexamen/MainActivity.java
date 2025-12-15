2ebc2d661fd3fd0735c8151fd5bd23c6e21a3287
Manu <dominguez.dieguez.manuel.jesus@iescamas.es>
2025-12-15 12:15:23 +0100

    ejercicio de repaso para subida nota

---
+++ RepasoExamen/app/src/main/java/com/example/repasoexamen/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.repasoexamen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCalcular;
+    private EditText txtPrecio;
+    private EditText txtDescuento;
+    private TextView txtFecha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initComponents();
+        calcularPrecio();
+        actualizarFechaHora();
+    }
+
+    private void initComponents() {
+        btnCalcular = findViewById(R.id.button);
+        txtPrecio = findViewById(R.id.editTextNumber2);
+        txtDescuento = findViewById(R.id.editTextNumber3);
+        txtFecha = findViewById(R.id.textView7);
+    }
+
+    private void actualizarFechaHora() {
+        Date ahora = new Date();
+        DateFormat formateador = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.MEDIUM);
+        String fechaAuto = formateador.format(ahora);
+        txtFecha.setText(fechaAuto);
+    }
+
+    private void calcularPrecio() {
+        btnCalcular.setOnClickListener(v -> {
+            try {
+                String precioStr = txtPrecio.getText().toString();
+                String descuentoStr = txtDescuento.getText().toString();
+
+                if (precioStr.isEmpty() || descuentoStr.isEmpty()) {
+                    Toast.makeText(this, "Debe de rellenar todos los campos.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int precio = Integer.parseInt(txtPrecio.getText().toString());
+                int descuento = Integer.parseInt(txtDescuento.getText().toString());
+
+                if (descuento < 0 || descuento > 100) {
+                    Toast.makeText(this, "El descuento no debe ser inferior a 0 ni superior a 100", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    double precioDesc = precio - (precio * descuento / 100);
+                    Intent intent = new Intent(MainActivity.this, Resultado.class);
+                    intent.putExtra("precioFinal", precioDesc);
+                    startActivity(intent);
+                }
+                actualizarFechaHora();
+            } catch (NumberFormatException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        actualizarFechaHora();
+    }
+}
---
+++ RepasoExamen/app/src/main/java/com/example/repasoexamen/Resultado.java
@@ -0,0 +1,114 @@
+package com.example.repasoexamen;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Map;
+
+public class Resultado extends AppCompatActivity {
+
+    public MainActivity main;
+    private TextView txtResultado;
+    private EditText txtComentario;
+    private Button btnGuardar;
+    private TextView comentario;
+    private TextView valoraciones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initComponents();
+        mostrarPrecio();
+        obtenerUltimaValoracion();
+        guardarComentario();
+        calcularMedia();
+    }
+
+    private void initComponents() {
+        txtResultado = findViewById(R.id.textView3);
+        txtComentario = findViewById(R.id.editTextTextMultiLine);
+        btnGuardar = findViewById(R.id.button2);
+        comentario = findViewById(R.id.textView5);
+        valoraciones = findViewById(R.id.textView6);
+    }
+
+    private void mostrarPrecio() {
+        double precio = getIntent().getDoubleExtra("precioFinal", 0);
+        String precioFormatted = NumberFormat.getCurrencyInstance().format(precio);
+        txtResultado.setText("El precio final es de " + precioFormatted);
+    }
+
+    private void guardarComentario() {
+        btnGuardar.setOnClickListener(v -> {
+            int valoracion = Integer.parseInt(txtComentario.getText().toString());
+
+            SharedPreferences prefs = getSharedPreferences("valoraciones", MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("ultima_valoracion", valoracion);
+
+            String claveUnica = "valoracion_" + System.currentTimeMillis();
+            editor.putInt(claveUnica, valoracion);
+
+            editor.apply();
+
+
+        });
+    }
+
+    private void obtenerUltimaValoracion() {
+        SharedPreferences prefs = getSharedPreferences("valoraciones", MODE_PRIVATE);
+        Map<String, ?> allEntries = prefs.getAll();
+
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+            if (entry.getKey().startsWith("ultima_")) {
+                Object value = (Integer) entry.getValue();
+                if (value instanceof Integer) {
+                    comentario.setText("La ultima valoracion es de: " + value);
+                }
+            }
+        }
+    }
+
+    private void calcularMedia () {
+        SharedPreferences prefs = getSharedPreferences("valoraciones", MODE_PRIVATE);
+        Map<String, ?> allEntries = prefs.getAll();
+
+        int suma = 0;
+        int contador = 0;
+        double resultado = 0;
+
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+            if (entry.getKey().startsWith("valoracion_")) {
+                Object value = (Integer) entry.getValue();
+                if (value instanceof Integer) {
+                    suma += (int) value;
+                    contador++;
+                }
+            }
+        }
+
+        resultado = suma / contador;
+
+        valoraciones.setText("La media de las valoraciones es de " + resultado);
+    }
+}
